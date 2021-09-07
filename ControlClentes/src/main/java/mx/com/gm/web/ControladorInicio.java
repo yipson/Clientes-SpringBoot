8b17a520027fccee208449f11325d5e21aa72262
Sebastian Valencia <sebastianvalencia.ceron@gmail.com>
2021-09-06 20:58:57 -0500

    Se agrega el envio de saldo total y total clientes desde el controlado, y estos datos de agregan a la pagina de index

--- ControlClentes/src/main/java/mx/com/gm/web/ControladorInicio.java
+++ ControlClentes/src/main/java/mx/com/gm/web/ControladorInicio.java
@@ -27,6 +27,12 @@ public class ControladorInicio {
         log.info("ejecutando el controlador Spring MVC");
         log.info("usuario que hizo login:" + user);
         model.addAttribute("personas", personas);
+        float saldoTotal=0F;
+        for(Persona p: personas){
+            saldoTotal += p.getSaldo();
+        }
+        model.addAttribute("saldoTotal", saldoTotal);
+        model.addAttribute("totalClientes", personas.size());
         return "index";
     }
     
